26f7879d6158265f53bcf71a3ae73c3be7328d01
SandraFre <sandra.email@gmail.com>
2021-11-29 14:21:37 +0200

    CommentService created

---
+++ src/main/java/com/example/blogapp/service/CommentService.java
@@ -0,0 +1,33 @@
+package com.example.blogapp.service;
+
+import com.example.blogapp.data.Comment;
+import com.example.blogapp.repository.CommentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class CommentService {
+    private final CommentRepository commentRepository;
+
+    public CommentService(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
+
+    public List<Comment> getComments(){
+        return commentRepository.findAll();
+    }
+
+    public Comment getComment(UUID id){
+        return commentRepository.findById(id).orElseThrow(()->new RuntimeException(String.valueOf(id)));
+    }
+
+    public void saveComment(Comment comment){
+        commentRepository.save(comment);
+    }
+
+    public void deleteComment(UUID id){
+        commentRepository.deleteById(id);
+    }
+}
